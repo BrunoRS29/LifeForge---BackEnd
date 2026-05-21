d9ff952e5f99fe731dec31227c3c504fae92ad7b
Remember22 <gabrielinnocencio22@gmail.com>
2026-05-21 14:52:21 -0300

    Update Application.kt
    Adicionado ML

--- src/main/kotlin/com/lifeforge/Application.kt
+++ src/main/kotlin/com/lifeforge/Application.kt
@@ -6,12 +6,16 @@ import com.lifeforge.plugins.configureHTTP
 import com.lifeforge.plugins.configureRouting
 import com.lifeforge.plugins.configureSecurity
 import com.lifeforge.plugins.configureSerialization
+import io.ktor.events.EventDefinition
 import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationStopped
 import io.ktor.server.netty.EngineMain
 
 /**
  * Entry point do servidor Ktor.
  * O `EngineMain.main` carrega application.conf e chama Application.module().
+ *
+ * Sprint 5: registra stop hook para fechar o MlClient (libera conexoes HTTP).
  */
 fun main(args: Array<String>) = EngineMain.main(args)
 
@@ -22,7 +26,11 @@ fun Application.module() {
     // 2. Container manual de dependencias
     val container = AppContainer(environment.config)
 
-    // 3. Plugins do Ktor
+    // 3. Stop hook - libera recursos do MlClient ao desligar o servidor.
+    //    Caso contrario, em deploy com hot-reload acumularia conexoes TCP.
+    monitor.subscribe(ApplicationStopped) { container.close() }
+
+    // 4. Plugins do Ktor
     configureSerialization()
     configureSecurity(container.jwtService)
     configureHTTP()
